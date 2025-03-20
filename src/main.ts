ef69aece0bd49069690a421f190261fa3d8e50b7
Amir Madaoui <amir.madaoui@epitech.eu>
2025-03-20 18:20:15 +0100

    [add] analyse in this page

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,15 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+router.afterEach((to) => {
+    if (window.gtag) {
+      window.gtag('config', 'G-EFQ0GRL5GB', { 
+        page_path: to.fullPath, 
+        debug_mode: true  // Active le mode debug
+      });
+    }
+  });
+
 app.use(router)
 app.mount('#app')
